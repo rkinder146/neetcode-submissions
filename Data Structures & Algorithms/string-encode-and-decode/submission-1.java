0d9e601f7003ad897bf82a0c6408017e996d23ea
Ryan Kinder <42724811+rkinder146@users.noreply.github.com>
2026-08-16 21:09:04 -0700

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder code = new StringBuilder();
+        code.append(threeCharacterInteger(strs.size()));
+        for (int i = 0; i<strs.size(); i++){
+            code.append(threeCharacterInteger(strs.get(i).length()));
+            code.append(strs.get(i));
+        }
+        return code.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> strs = new ArrayList<String>();
+        int size = Integer.parseInt(str.substring(0, 3));
+        int last = 3;
+        System.out.print(str);
+        for (int i=0; i<size; i++){
+            int length = Integer.parseInt(str.substring(last, last + 3));
+            strs.add(i, str.substring(last + 3, last + length + 3));
+            last += length + 3;
+        }
+        return strs;
+    }
+
+    private String threeCharacterInteger(int x){
+        return ""  + (x/100)%10 + (x/10)%10 + x%10;
+    }   
+}
